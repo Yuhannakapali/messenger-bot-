aca5611d17bc0022999092b866e599300bb41dae
upeshchalise <upeshchalise@gmail.com>
2023-10-30 16:07:35 +0545

    first commit

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/webhook", (req, res) => {
+app.get("/webhook", (req, res) => {
   let body = req.body;
+  console.log(body);
+  // send a '200 OK' response if this page is webhook
+  //   if (body.object === "page") {
+  //     //   returns '200 OK' response to all requests
+  //     res.status(200).send("Event Received");
+  //   } else {
+  //     res.status(404).send("Event Not Found");
+  //   }
 
-  console.log(`\u{1F7EA} Received webhook`);
-  console.dir(body, { depth: null });
-  res.send(`\u{1F7EA} Response from server`);
+  //   console.log(`\u{1F7EA} Received webhook`);
+  //   console.dir(body, { depth: null });
+  //   res.send(` Response from server`);
 });
 
 app.listen(port, () => {
