b5ab451ce1463d67ceaf8759cffe681d91a209a9
upeshchalise <upeshchalise@gmail.com>
2023-10-31 13:23:16 +0545

    check

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
-const express = require("express");
+const express = require("express"),
+  { urlencoded, json } = require("body-parser"),
+  crypto = require("crypto"),
+  path = require("path");
+
 const dotenv = require("dotenv").config();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3001;
+
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+
+// Parse application/json. Verify that callback came from Facebook
+// app.use(json({ verify: verifyRequestSignature }));
+
+// Serving static files in Express
+app.use(express.static(path.join(path.resolve(), "public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Upesh!");
 });
 
-app.get("/webhook", (req, res) => {
-  let body = req.body;
-  // console.log(body);
-  // send a '200 OK' response if this page is webhook
-  //   if (body.object === "page") {
-  //     //   returns '200 OK' response to all requests
-  //     res.status(200).send("Event Received");
-  //   } else {
-  //     res.status(404).send("Event Not Found");
-  //   }
+// app.post("/webhook", (req, res) => {
+//   let body = req.body;
 
-  //   console.log(`\u{1F7EA} Received webhook`);
-  //   console.dir(body, { depth: null });
-  //   res.send(` Response from server`);
+//   console.log(`\u{1F7EA} Received webhook:`);
+//   console.dir(body, { depth: null });
+// });
+
+// Add support for GET requests to our webhook
+app.get("/webhook", (req, res) => {
+  // parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // check if mode and token sent is correct
+    if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+      // respond with the challenge token from the request
+      console.log("Webhook verified");
+      res.status(200).send(challenge);
+    } else {
+      // respond with '403 forbidden' if verify token do not match
+      res.status(403);
+    }
+  }
 });
 
 app.listen(port, () => {
