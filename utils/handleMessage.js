f9c9df0aaaf1d2a2cb94125e0b312ec0f43272ab
upeshchalise <upeshchalise@gmail.com>
2023-11-01 15:37:23 +0545

    adding features to the webandapp pages

--- utils/handleMessage.js
+++ utils/handleMessage.js
@@ -1,5 +1,6 @@
 const { callSendAPI } = require("./callSendAPI");
 const { randomJokes } = require("./randomJokes");
+const fs = require("fs");
 
 // function handleMessage(senderPsid, receivedMessage) {
 const handleMessage = async (senderPsid, receivedMessage) => {
@@ -7,18 +8,29 @@ const handleMessage = async (senderPsid, receivedMessage) => {
 
   // Checks if the message contains text
   if (receivedMessage.text) {
+    let userMessage = fs.readFileSync(
+      `${__dirname}/../config/services.json`,
+      "utf8"
+    );
+
+    // if (!userMessage[receivedMessage.text] === undefined) {
+    //   console.log("test message received");
+    // }
+    if (
+      Object.prototype.hasOwnProperty.call(userMessage, receivedMessage.text)
+    ) {
+      console.log("jfdkj");
+    }
     if (receivedMessage.text.includes("hi")) {
       response = {
-        text: `hello there my friend`,
+        text: `Hello, we are always available for your services`,
       };
-      console.log("running hi");
     } else if (receivedMessage.text.includes("jokes")) {
       let jokes;
       jokes = await randomJokes();
       response = {
         text: `${jokes}`,
       };
-      console.log("running jokes");
     } else {
       response = {
         text: `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`,
